c58b22016b5f7acb49ecdaea094cf805d1b9e9bf
Aaron Dave Siapuatco <aarondavesiapuatco@Aarons-MacBook-Air.local>
2025-04-17 23:58:28 +0800

    fix: fixed path issues

--- webpack.config.js
+++ webpack.config.js
@@ -14,10 +14,19 @@ module.exports = {
   plugins: [
     new CopyWebpackPlugin({
       patterns: [
-        { from: "src/manifest.json", to: "manifest.json" },
-        { from: "src/popup/popup.html", to: "popup.html" },
-        { from: "src/popup/popup.css", to: "popup.css" },
-        { from: "src/images", to: "images" },
+        {
+          from: path.resolve(__dirname, "src/manifest.json"),
+          to: "manifest.json",
+        },
+        {
+          from: path.resolve(__dirname, "src/popup/popup.html"),
+          to: "popup/popup.html",
+        },
+        {
+          from: path.resolve(__dirname, "src/popup/popup.css"),
+          to: "popup/popup.css",
+        },
+        { from: path.resolve(__dirname, "src/images"), to: "images" },
       ],
     }),
   ],
