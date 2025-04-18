2980c548db953bb52fe59b0ca667384f977954f2
44mira <legolaslada@gmail.com>
2025-04-18 17:52:17 +0800

    feat: readability scrape

--- src/scripts/content.js
+++ src/scripts/content.js
@@ -1,1 +1,15 @@
 // Access the DOM (Scraping part)
+
+let { Readability } = require("@mozilla/readability");
+
+let _document = document.cloneNode(true);
+let { title, textContent, siteName } = new Readability(_document).parse();
+let URL = window.location.href;
+
+let text_output = JSON.stringify(
+  { url: URL, title, textContent, siteName },
+  null,
+  2,
+);
+
+console.log(text_output);
